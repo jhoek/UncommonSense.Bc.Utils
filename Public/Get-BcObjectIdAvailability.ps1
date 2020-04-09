e297bbb1d1784a9c4064a426495ad74bb75d27db
Jan Hoek <github@uncommonsense.nl>
2020-04-09 19:31:40 +0200

    More work on the summaries

--- Public/Get-BcObjectIdAvailability.ps1
+++ Public/Get-BcObjectIdAvailability.ps1
@@ -23,22 +23,42 @@ function Get-BcObjectIdAvailability
 
     if ($Summary)
     {
-        
-    }
-    else
-    {
+        $AllIds = @{ }
+
         $IdRangeResult `
         | ForEach-Object {
             $CurrentRange = $_
 
             ($CurrentRange.From)..($CurrentRange.To) `
             | ForEach-Object {
-                [UncommonSense.Bc.Utils.ObjectIdAvailability]::new(
-                    $CurrentRange.Type,
-                    $_,
-                    [UncommonSense.Bc.Utils.Availability]::InUse
-                )            
+                $CurrentID = $_
+
+                if (-not $AllIds.ContainsKey($CurrentID))
+                {
+                    $AllIds[$CurrentID] = @{
+                        [UncommonSense.Bc.Utils.ObjectType].GetEnumValues()
+                    }
+
+                }
             }
+
+        $AllIds
     }
+}
+else
+{
+    $IdRangeResult `
+    | ForEach-Object {
+        $CurrentRange = $_
+
+        ($CurrentRange.From)..($CurrentRange.To) `
+        | ForEach-Object {
+            [UncommonSense.Bc.Utils.ObjectIdAvailability]::new(
+                $CurrentRange.Type,
+                $_,
+                [UncommonSense.Bc.Utils.Availability]::InUse
+            )
+        }
+}
 }
 }
