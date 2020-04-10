7a2d837180fd6c3169681d07c65184aaf01c4aa1
Jan Hoek <github@uncommonsense.nl>
2020-04-10 07:20:13 +0200

    Slashed out for now

--- Cmdlets/FindAvailableBcObjectIdCmdlet.cs
+++ Cmdlets/FindAvailableBcObjectIdCmdlet.cs
@@ -1,28 +1,28 @@
-using System.Management.Automation;
+// using System.Management.Automation;
 
-namespace UncommonSense.Bc.Utils
-{
-    [Cmdlet(VerbsCommon.Find, "AvailableBcObjectId")]
-    [OutputType(typeof(ObjectIdInfo))]
-    public class FindAvailableBcObjectIdCmdlet : Cmdlet
-    {
-        [Parameter(Position = 0)]
-        [ValidateNotNullOrEmpty()]
-        public string Path { get; set; } = ".";
+// namespace UncommonSense.Bc.Utils
+// {
+//     [Cmdlet(VerbsCommon.Find, "AvailableBcObjectId")]
+//     [OutputType(typeof(ObjectIdInfo))]
+//     public class FindAvailableBcObjectIdCmdlet : Cmdlet
+//     {
+//         [Parameter(Position = 0)]
+//         [ValidateNotNullOrEmpty()]
+//         public string Path { get; set; } = ".";
 
-        [Parameter()]
-        [ValidateNotNull()]
-        public ScriptBlock GetObjectIdAvailabity { get; set; } = ScriptBlock.Create("param([string]$Path) Get-BcObjectIdAvailability -Path $Path");
+//         [Parameter()]
+//         [ValidateNotNull()]
+//         public ScriptBlock GetObjectIdAvailabity { get; set; } = ScriptBlock.Create("param([string]$Path) Get-BcObjectIdAvailability -Path $Path");
 
-        [Parameter()]
-        [ValidateRange(0, int.MaxValue)]
-        public int Tables { get; set; }
+//         [Parameter()]
+//         [ValidateRange(0, int.MaxValue)]
+//         public int Tables { get; set; }
 
-        [Parameter()]
-        [ValidateRange(0, int.MaxValue)]
-        public int Pages { get; set; }
+//         [Parameter()]
+//         [ValidateRange(0, int.MaxValue)]
+//         public int Pages { get; set; }
 
-        // use lowest id first, contiguous range per object type, same ids for all object types        
+//         // use lowest id first, contiguous range per object type, same ids for all object types
 
-    }
-}
+//     }
+// }
